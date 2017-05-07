67cc7b2719967afcce1c394956c28bc33fbcf46b
Joe Wang <wangqiao11@hotmail.com>
2017-05-07 16:06:39 +0800

    add basic combinators

--- src/combinators.lisp
+++ src/combinators.lisp
@@ -4,25 +4,53 @@
 ;; (defun parser (a)
 ;;   (lambda (string)
 ;;     ...
-;;     '(a, string)))
+;;     (a string)))
 
 
 (defun unit (a)
   "unit :: a -> Parser a"
-  (lambda (str)
-    (list (cons a str))))
+  #'(lambda (s)
+    (values a s)))
 
-(defun item ()
-  (lambda (str)
-    (let ((c (aref str 0))
-          (cs (subseq str 1)))
-      (list (cons c cs)))))
+(defun item (s)
+  "item :: String -> (Char, String)"
+  (let ((c (get-first s))
+        (cs (get-rest s)))
+    (values c cs)))
 
 
 (defun bind (p f)
-  (lambda (str)
-    (destructuring-bind
-        ((a . rest))
-        (parse p str)
-      (let ((p2 (f a)))
-        '(,p2 ,rest)))))
+  "bind :: Parser a -> (a -> Parser b) -> Parser b"
+  #'(lambda (s)
+    (multiple-value-bind (a rest) (parse p s)
+      (multiple-value-bind (a1 rest1) (parse (funcall f a) rest)
+        (values a1 rest1)))))
+
+(defun >>= (p f)
+  "alias for bind"
+  (bind p f))
+
+(defun >> (pa pb)
+  ">> :: Parser a -> Parser b -> Parser b"
+  #'(lambda (s)
+    (parse (bind pa #'(lambda (_)
+                        (declare (ignore _))
+                        pb))
+           s)))
+
+
+(defmacro mdo (binds &body body)
+  "(>= ma (lambda (a)
+             (>= mb (lambda (b)
+                       (...)))))"
+  (if (null binds)
+      `(funcall #'(lambda () ,@body))
+      (let* ((bind (car binds))
+             (rest (cdr binds))
+             (var (car bind))
+             (m (cadr bind)))
+        (format t "~a ~a ~a" m var bind)
+        (if (eql var '_)
+            `(>> ,m (mdo ,rest ,@body))
+            `(>>= ,m #'(lambda (,var)
+                         (mdo ,rest ,@body)))))))
--- src/prim.lisp
+++ src/prim.lisp
@@ -1,5 +1,17 @@
 (in-package :cl-parsec)
 
+(defmethod get-first ((l list))
+  (car l))
+
+(defmethod get-first ((s string))
+  (aref s 0))
+
+(defmethod get-rest ((l list))
+  (cdr l))
+
+(defmethod get-rest ((s string))
+  (subseq s 1))
+
 (defun parse (parser input)
   "Parser a -> (a, string)"
   (funcall parser input))
